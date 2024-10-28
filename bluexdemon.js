90c3e2e0da68a5c192cfdf1241b7e5dc64bb0f91
BLUEXDEMONl <bolaolat3000@gmail.com>
2024-10-28 22:34:13 +0100

    Add files via upload

--- bluexdemon.js
+++ bluexdemon.js
@@ -456,6 +456,75 @@ reject(e)
 if (!byxx.public) {
 if (!m.key.fromMe) return
 } 
+async function getAccessToken() {
+    try {
+        const client_id = 'acc6302297e040aeb6e4ac1fbdfd62c3';
+        const client_secret = '0e8439a1280a43aba9a5bc0a16f3f009';
+        const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+        const response = await axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
+            headers: {
+                Authorization: `Basic ${basic}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        const data = response.data;
+        return data.access_token;
+    } catch (error) {
+        console.error('Error getting Spotify access token:', error);
+        throw 'An error occurred while obtaining Spotify access token.';
+    }
+}
+async function spotifydl(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const kemii = await axios.get(
+        `https://api.fabdl.com/spotify/get?url=${encodeURIComponent(url)}`,
+        {
+          headers: {
+            accept: "application/json, text/plain, */*",
+            "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"24\", \"Chromium\";v=\"116\"",
+            "sec-ch-ua-mobile": "?1",
+            "sec-ch-ua-platform": "\"Android\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            Referer: "https://spotifydownload.org/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+          },
+        }
+      );
+      const kemi = await axios.get(
+        `https://api.fabdl.com/spotify/mp3-convert-task/${kemii.data.result.gid}/${kemii.data.result.id}`,
+        {
+          headers: {
+            accept: "application/json, text/plain, */*",
+            "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "sec-ch-ua": "\"Not)A;Brand\";v=\"24\", \"Chromium\";v=\"116\"",
+            "sec-ch-ua-mobile": "?1",
+            "sec-ch-ua-platform": "\"Android\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "cross-site",
+            Referer: "https://spotifydownload.org/",
+            "Referrer-Policy": "strict-origin-when-cross-origin",
+          },
+        }
+      );
+      const result = {};
+      result.title = kemii.data.result.name;
+      result.type = kemii.data.result.type;
+      result.artis = kemii.data.result.artists;
+      result.durasi = kemii.data.result.duration_ms;
+      result.image = kemii.data.result.image;
+      result.download = "https://api.fabdl.com" + kemi.data.result.download_url;
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
 async function searchSpotify(query) {
     try {
         const access_token = await getAccessToken();
